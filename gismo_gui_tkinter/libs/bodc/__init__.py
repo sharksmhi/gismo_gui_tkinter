0c5848bd5eda2d187252ca5db83f175c738f2184
magnuswenzer <magnus@wenzer.se>
2018-11-07 14:06:10 +0100

    remove tree

--- gismo_gui_tkinter/libs/bodc/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2018 SMHI, Swedish Meteorological and Hydrological Institute
-# License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
-"""
-Created on Thu Aug 30 15:53:08 2018
-
-@author: a001985
-"""
-from . import vocabulary
